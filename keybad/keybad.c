f202367e5d48b86e3e415e310f896a23cf2fee70
hesham-salem <hesham1salem@gmail.com>
2021-05-24 20:17:02 +0200

    intial keybad driver

---
+++ keybad/keybad.c
@@ -0,0 +1,18 @@
+/*  module name :keypad driver
+ *
+ *  file name :keybad.c
+ *
+ *  description :the source file of keypad driver
+ *
+ *  Created on: May 24, 2021
+ *
+*   Author: Hesham Salem
+ */
+
+#include"keybad.h"
+
+/***************************************************************
+ * 					function definition 						*
+ * *************************************************************
+ */
+
---
+++ keybad/keybad.h
@@ -0,0 +1,18 @@
+/*  module name :keypad driver
+ *
+ *  file name :keybad.h
+ *
+ *  description :the header file of keypad driver
+ *
+ *  Created on: May 24, 2021
+ *
+*   Author: Hesham Salem
+ */
+#ifndef KEYBAD_H_
+#define KEYBAD_H_
+#include"std_types.h"
+#include"micro_config.h"
+#include"common_macros.h"
+
+
+#endif /* KEYBAD_H_ */
