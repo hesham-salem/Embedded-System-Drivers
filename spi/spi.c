e3070eaee1a1a63f9897ad1baaa8d146fa255096
hesham-salem <hesham1salem@gmail.com>
2021-05-31 00:01:22 +0200

    solution of bug

--- spi/spi.c
+++ spi/spi.c
@@ -9,22 +9,33 @@
 
 void SPI_initMaster(void)
 {
-DDRB|=(1<<PB4)|(1<<PB5)|(1<<PB7);
-DDRB&=~(1<<PB6);
-//SET_BIT(SPCR,SPE);
-//sET_BIT(SPCR,MSTR);
-//SET_BIT(SPCR,SPR0);
-SPCR=(SPE)|(MSTR);
+	/******** Configure SPI Master Pins *********
+	 * SS(PB4)   --> Output
+	 * MOSI(PB5) --> Output
+	 * MISO(PB6) --> Input
+	 * SCK(PB7) --> Output
+	 ********************************************/
+	DDRB = DDRB | (1<<PB4);
+	DDRB = DDRB | (1<<PB5);
+	DDRB = DDRB & ~(1<<PB6);
+	DDRB = DDRB | (1<<PB7);
+
+	SPCR = (1<<SPE) | (1<<MSTR); // enable SPI + enable Master + choose SPI clock = Fosc/4
 }
+
 void SPI_initSlave(void)
 {
-	DDRB&=~(1<<PB4)&~(1<<PB5)&~(1<<PB7);
-	DDRB|=(1<<PB6);
-	//SET_BIT(SPCR,SPE);
-	//CLEAR_BIT(SPCR,MSTR);
-	//SET_BIT(SPCR,SPR0);
-	SPCR=(1<<SPE);
-
+	/******** Configure SPI Slave Pins *********
+	 * SS(PB4)   --> Input
+	 * MOSI(PB5) --> Input
+	 * MISO(PB6) --> Output
+	 * SCK(PB7) --> Input
+	 ********************************************/
+	DDRB = DDRB & (~(1<<PB4));
+	DDRB = DDRB & (~(1<<PB5));
+	DDRB = DDRB | (1<<PB6);
+	DDRB = DDRB & (~(1<<PB7));
+	SPCR = (1<<SPE); // just enable SPI + choose SPI clock = Fosc/4
 }
 void SPI_sendByte(const uint8 data)
 {
