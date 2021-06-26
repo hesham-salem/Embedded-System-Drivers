cba5f13cf394de5d5ad8cbaeb5838f1ee879708d
Hesham Salem <36461250+hesham-salem@users.noreply.github.com>
2021-06-26 18:46:07 +0200

    final and tested

--- i2c/i2c.c
+++ i2c/i2c.c
@@ -1,46 +1,58 @@
-/*
- * i2c.c
+/*  module name :i2c driver
  *
- *  Created on: Jun 6, 2021
- *      Author: hesham
+ *  file name :timer.c
+ *
+ *  description :the source file of I2C driver
+ *
+ *
+ *   Author: Hesham Salem
  */
 #include"i2c.h"
+/***************************************************************
+ * 					functions definition 			*
+ * *************************************************************
+ */
 
 void TWI_init(const TWI_configurationType *config)
 {
-	//TWBR=0X02;//buad rate
+	/* the equation is SCL frequency = CPU clock frequency /(16+2(TWBR)*4^prescaler */
 	TWBR=((float)F_CPU/config->baudRate +16)/(2*pow(4,config->prescasler));
-	TWSR=0X00; //prescaler
-	TWSR=config->prescasler;
-	 TWAR=config->slave_address<<1;// adress+ other bit
-	TWCR|=(1<<TWEN);// دائما فى الاخر
+	 	TWSR=config->prescasler;/*store prescaler  in first 2 bits (TWPS1 and TWPS0) as other bits read only*/
+	 TWAR=config->slave_address<<1;/* store slave address (7bits) in the most 7 significant bits
+	  the address of master when receive data from (as slave) form other slaves */
+	TWCR|=(1<<TWEN);/* enable TWI module */
 
 }
 void TWI_start(void)
 {
 	//make enable again to force other bits in the register to be zero
-	//start bit and clear done flag
+	//start bit TWSTA and clear finish current job flag TWINT by toggle it
 TWCR=(1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-while(BIT_IS_CLEAR(TWCR,TWINT));// WAIT acation done
+while(BIT_IS_CLEAR(TWCR,TWINT));// WAIT finish current job  flag TWINT =1
 }
 void TWI_stop(void)
-{
+{/*make enable again to force other bits in the register to be zero
+	stop bit TWST0 and clear finish current job flag TWINT by toggle it */
 TWCR=(1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
 
-//NO NEED TO WAIT twint flag
+//NO NEED TO WAIT TWINT flag as it  is the end of frame
 }
 void TWI_write(uint8 data)
 {
-
+/* store data in TWDR */
 	TWDR=data;
-
+	/*make enable again to force other bits in the register to be zero
+		 clear finish current job flag TWINT by toggle it */
 	TWCR=(1<<TWINT)|(1<<TWEN);
 
-	while(BIT_IS_CLEAR(TWCR,TWINT));
+	while(BIT_IS_CLEAR(TWCR,TWINT));// WAIT finish current job  flag TWINT =1
 
 }
 uint8 TWI_read_with_ACK(void)
-{
+{/*make enable again to force other bits in the register to be zero
+		 clear finish current job flag TWINT by toggle it
+		 enable acknowledge bit TWEA */
+
 	TWCR=(1<<TWINT)|(1<<TWEA)|(1<<TWEN);
 	while(BIT_IS_CLEAR(TWCR,TWINT));
 	//CLEAR flag before or after
@@ -48,13 +60,14 @@ uint8 TWI_read_with_ACK(void)
 }
 
 uint8 TWI_read_with_NACK(void)
-{
+{/*make enable again to force other bits in the register to be zero
+		 clear finish current job flag TWINT by toggle it*/
 	TWCR=(1<<TWINT)|(1<<TWEN);
-	while(BIT_IS_CLEAR(TWCR,TWINT));
-	//CLEAR flag before or after
+	while(BIT_IS_CLEAR(TWCR,TWINT));// WAIT finish current job  flag TWINT =1
+
 	return TWDR;
 }
 uint8 TWI_getStatus()
-{
+{	/*return bits 7:3 in status register TWSR */
 	return TWSR&0XF8;
 }
