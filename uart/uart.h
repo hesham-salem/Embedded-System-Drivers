1b67f91c6f55bec82d236b8f135514eb360b7ba0
Hesham Salem <36461250+hesham-salem@users.noreply.github.com>
2021-06-26 18:44:23 +0200

    Update uart.h

--- uart/uart.h
+++ uart/uart.h
@@ -1,29 +1,44 @@
-/*
- * uart.h
+/*  module name :uart driver
  *
- *  Created on: May 29, 2021
- *      Author: hesham
+ *  file name :uart.h
+ *
+ *  description :the header file of uart driver
+ *
+ *  Created on: May 24, 2021
+ *
+ *   Author: Hesham Salem
  */
-
 #ifndef UART_H_
 #define UART_H_
 #include "common_macros.h"
 #include "micro_config.h"
 #include "std_types.h"
 
+/***************************************************************
+ * 		structure used in UART configuration 						*
+ * *************************************************************
+ */
 typedef struct
 {
-	 uint8 character_size;
+	 uint8 character_size; /* 5,6,7,8 or 9 data bits */
 	 uint32 buadRate;
-	 char *mode;
+	 char mode[20]; /* string copy --->>("normal" or "double speed" ) */
+	 char parity[20];/* 1 or 2*/
+	 uint8 stop_bit_number /* string copy --->>("odd" ,"even" or " disable" ) */
 }UART_configurationType;
-typedef uint8 character;
+
+
+
+/***************************************************************
+ * 					functions prototyping 						*
+ * *************************************************************
+ */
 
 void UART_init(UART_configurationType * config);
-uint8 UART_recieveByte(void);
+uint8 UART_receiveByte(void);
 void UART_sendByte(const uint8 data);
 void UART_sendString(uint8 *str);
-void UART_recieveString(uint8 *str);
+void UART_receiveString(uint8 *str);
 
 
 #endif /* UART_H_ */
